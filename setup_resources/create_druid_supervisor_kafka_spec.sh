7721458ee9ba7ac1b22fcb930006c88482371cbf
Ashish Narasimham <ashish.narasimham@gmail.com>
2018-03-01 18:05:18 -0500

    file name change

--- setup_resources/create_druid_supervisor_kafka_spec.sh
+++ setup_resources/create_druid_supervisor_kafka_spec.sh
@@ -1,4 +1,4 @@
-curl -X POST -H 'Content-Type: application/json' -d @../druid/ingestion_specs/supervisor_kafka.json http://<HOSTNAME>:8090/druid/indexer/v1/supervisor
+curl -X POST -H 'Content-Type: application/json' -d @../druid/ingestion_specs/manf_data_kafka.json http://<HOSTNAME>:8090/druid/indexer/v1/supervisor
 
 curl -X POST -H 'Content-Type: application/json' -d @../druid/ingestion_specs/averages_superv.json http://<HOSTNAME>:8090/druid/indexer/v1/supervisor
 
